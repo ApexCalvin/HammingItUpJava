93d520dc661cba26c6f45ff82f4f0ef77b5109f4
Calvin <calvin@Zipcoders-MacBook-Pro.local>
2022-11-29 22:15:35 -0500

    commented code

--- src/main/zipcode/Hamming.java
+++ src/main/zipcode/Hamming.java
@@ -1,31 +1,30 @@
 package src.main.zipcode;
 
 public class Hamming {
-
     String s;
     String s1;
+
     public Hamming(String s, String s1) {
-        if(!(s.length() == s1.length())) {
+        //if the length of two strings don't equal
+        if(s.length() != s1.length()) {
+            //sout error message
             throw new IllegalArgumentException("leftStrand and rightStrand must be of equal length.");
         } else {
+            //if they do equal, set
             this.s = s;
             this.s1 = s1;
         }
-
     }
 
     public int getHammingDistance() {
         int count = 0;
-        String[] sArray = s.split("");
-        String[] s1Array = s1.split("");
+        //iterate through both strings and compare chars
         for (int i = 0; i < s.length(); i++) {
-            if(!sArray[i].equals((s1Array[i]))) {
+            //if they're not the same, count it
+            if (s.charAt(i) != s1.charAt(i)) {
                 count++;
             }
-
         }
         return count;
     }
 }
-
-// THANKS
