e31514ce609a10edfbcac654fa266819c1177625
Calvin <calvin@Zipcoders-MacBook-Pro.local>
2022-11-29 21:37:22 -0500

    completed

--- src/main/zipcode/Hamming.java
+++ src/main/zipcode/Hamming.java
@@ -1,10 +1,31 @@
-package zipcode;
+package src.main.zipcode;
 
 public class Hamming {
+
+    String s;
+    String s1;
     public Hamming(String s, String s1) {
+        if(!(s.length() == s1.length())) {
+            throw new IllegalArgumentException("leftStrand and rightStrand must be of equal length.");
+        } else {
+            this.s = s;
+            this.s1 = s1;
+        }
+
     }
 
     public int getHammingDistance() {
-        return -1;
+        int count = 0;
+        String[] sArray = s.split("");
+        String[] s1Array = s1.split("");
+        for (int i = 0; i < s.length(); i++) {
+            if(!sArray[i].equals((s1Array[i]))) {
+                count++;
+            }
+
+        }
+        return count;
     }
 }
+
+// THANKS
